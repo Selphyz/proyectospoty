81b10c79b207aef5a7533fd88b9055a4e8ffeb18
matejadas <matejadas@outlook.com>
2019-08-26 13:55:56 +0200

    rutas ok

--- src/Controller/BibliotecaController.php
+++ src/Controller/BibliotecaController.php
@@ -16,4 +16,29 @@ class BibliotecaController extends AbstractController
             'controller_name' => 'BibliotecaController',
         ]);
     }
+
+    /**
+     * @Route("/listaCanciones", name="listaCanciones")
+     */
+    public function listaCanciones()
+    {
+        $res=[
+            [
+                'title'=>'Rave Digger',
+                'file'=> 'rave_digger',
+                'howl'=> null
+            ],[
+                'title'=> '80s Vibe',
+                'file'=> '80s_vibe',
+                'howl'=> null
+            ],[
+                'title'=> 'Running Out',
+                'file'=> 'running_out',
+                'howl'=> null
+            ]
+        ];
+        return $this->json($res);
+    }
+
+    
 }
