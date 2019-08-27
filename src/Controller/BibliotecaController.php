94f58a7077fff01f43a9f63872426d7eccdb1885
Carlos Dominguez <carlos.dom13@hotmail.com>
2019-08-27 13:57:32 +0200

    insertar lista-canciones

--- src/Controller/BibliotecaController.php
+++ src/Controller/BibliotecaController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Listas;
+use App\Entity\Usuarios;
 use App\Entity\Canciones;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -20,6 +23,33 @@ class BibliotecaController extends AbstractController
             'canciones'=>$canciones,
         ]);    
     }
+
+    /**
+    * @Route("/addCancion/{id}/{idLista}", name="addCancion")
+    */
+    public function addCancion(Canciones $cancion, int $idLista, EntityManagerInterface $em){
+
+        //$user=$this->getUser();
+        //$user=$em->getReference(Usuarios::class, 1);
+        $lista=$em->getReference(Listas::class, $idLista);
+
+        //$user->addCancion($cancion);
+        $lista->addIdCancion($cancion);
+
+        //$em->persist($user);
+        $em->persist($lista);
+        $em->flush();
+
+        $this->redirectToRoute("verLista");
+/*
+        $repo=$this->getDoctrine()->getRepository(Canciones::class);
+        $canciones=$repo->leerCanciones();
+
+        return $this->render('biblioteca/index.html.twig',[
+            'canciones'=>$canciones,
+        ]);    */
+    }
+
 
     /**
      * @Route("/listaCanciones", name="listaCanciones")
