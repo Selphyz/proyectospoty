b569bf7040d94276c7b6cb5034413f4abdbc6660
matejadas <matejadas@outlook.com>
2019-08-29 13:54:15 +0200

    player

--- public/js/principal.js
+++ public/js/principal.js
@@ -1,17 +1,10 @@
 var ns = {
 
-    canciones: ['cancion1.mp3', 'cancion2.mp3'],
+    canciones: ['AC DC - Highway To Hell (1979) - 03 - Walk All Over You.mp3', 'cancion2.mp3'],
     activa: 0,
     play : function(){
-        $('#player').attr('src', ns.canciones[ns.activa])
+        $('#player').attr('src', 'assets/audio/' + ns.canciones[ns.activa] + '?rand=' + Math.random());
         $('#player').trigger('play');
-    },
-    next : function(){
-        ns.activa=ns.activa+1;
-        if (ns.activa==ns.canciones.length){
-            ns.activa=0;
-        }
-        ns.play();
     },
     previous : function(){
         ns.activa=ns.activa-1;
@@ -20,12 +13,26 @@ var ns = {
         }
         ns.play();
     },
-    goTo: function(obj){
-        ns.activa=obj.getAttribute('numeroCancion');
+    next : function(){
+        ns.activa=ns.activa+1;
+        if (ns.activa==ns.canciones.length){
+            ns.activa=0;
+        }
+        alert(ns.activa);
         ns.play();
     },
+    // goTo: function(obj){
+    //     ns.activa=obj.getAttribute('numeroCancion');
+    //     ns.play();
+    // },
     stop: function(){
         $('#player').trigger('stop');
+    },
+    recargarCanciones(){
+        /*canciones=[];
+        $('#claseCancion').forEach(function(obj){
+            array_push(obj.getAttribute('ruta'));
+        }*/
     }
 }
 
@@ -36,8 +43,14 @@ $(document).ready(function(){
     $('#btnIniciar').on('click', function(){
         ns.play();
     });
+    $('#btnPlay').on('click', function(){
+        ns.play();
+    });
     $('#player').on('ended', function(){
         ns.next();
+    })
+    $('#btnPrevious').on('click', function(){
+        ns.previous();
     })
     $('#btnNext').on('click', function(){
         ns.next();
