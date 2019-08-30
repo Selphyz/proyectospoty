6c8d060a66c736803c45def9794c0a8474a9e79f
Carlos Dominguez <carlos.dom13@hotmail.com>
2019-08-30 02:12:55 +0200

    botones y reproduccion

--- public/js/principal.js
+++ public/js/principal.js
@@ -1,24 +1,37 @@
+var listado=$(".nombre").map(function(index, element){
+    return element.innerText;
+});
+console.log(listado);
 var ns = {
-
-    canciones: ['AC DC - Highway To Hell (1979) - 03 - Walk All Over You.mp3', 'cancion2.mp3'],
+    // canciones: ['AC DC - Highway To Hell (1979) - 03 - Walk All Over You.mp3', 'cancion2.mp3'],
     activa: 0,
     play : function(){
-        $('#player').attr('src', 'assets/audio/' + ns.canciones[ns.activa] + '?rand=' + Math.random());
+        $('#player').attr('src', 'assets/audio/' + listado[ns.activa] + '?rand=' + Math.random());
         $('#player').trigger('play');
     },
     previous : function(){
-        ns.activa=ns.activa-1;
+        ns.activa=parseInt(ns.activa)-1;
         if (ns.activa<0){
             ns.activa=0;
         }
         ns.play();
     },
     next : function(){
-        ns.activa=ns.activa+1;
-        if (ns.activa==ns.canciones.length){
+        ns.activa=parseInt(ns.activa)+1;
+        console.log("assets/audio/"+listado[ns.activa]);
+        if (ns.activa===listado.length){
             ns.activa=0;
         }
-        alert(ns.activa);
+        console.log(ns.activa);
+        ns.play();
+    },
+    especifica : function(cancion){
+        console.log(cancion);
+        ns.activa=parseInt(cancion);
+        console.log(ns.activa);
+        // if (ns.activa==listado.length){
+        //     ns.activa=0;
+        // }
         ns.play();
     },
     // goTo: function(obj){
@@ -34,31 +47,28 @@ var ns = {
             array_push(obj.getAttribute('ruta'));
         }*/
     }
-}
+};
 
 $(document).ready(function(){
-    $('#btnBuscar').on("click", function(){
-        ns.buscar();
-    })
-    $('#btnIniciar').on('click', function(){
-        ns.play();
+    $('.playcancion').on('click', function(){
+        ns.especifica($(this).attr('cancion'));
     });
     $('#btnPlay').on('click', function(){
         ns.play();
     });
     $('#player').on('ended', function(){
         ns.next();
-    })
+    });
     $('#btnPrevious').on('click', function(){
         ns.previous();
-    })
+    });
     $('#btnNext').on('click', function(){
         ns.next();
-    })
+    });
     $('#btnStop').on('click', function(){
         ns.stop();
-    })
+    });
     $('.btn-song').on('click', function(){
         ns.goTo(this);
     });
-})
+});
