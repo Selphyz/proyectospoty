a98ab839aa5d64e1581b83729eadcb6490a09c57
Carlos Dominguez <carlos.dom13@hotmail.com>
2019-08-30 13:59:31 +0200

    botones reproduccion pausa y stop

--- public/js/principal.js
+++ public/js/principal.js
@@ -1,10 +1,22 @@
 var listado=$(".nombre").map(function(index, element){
     return element.innerText;
 });
+var s=true;
 console.log(listado);
 var ns = {
     // canciones: ['AC DC - Highway To Hell (1979) - 03 - Walk All Over You.mp3', 'cancion2.mp3'],
     activa: 0,
+    pause : function(){
+        if(s)
+        {
+            $("#player").trigger('pause');
+            s=false;
+        }
+        else {
+            $("#player").trigger('play');
+            s=true;
+        }
+    },
     play : function(){
         $('#player').attr('src', 'assets/audio/' + listado[ns.activa] + '?rand=' + Math.random());
         $('#player').trigger('play');
@@ -29,9 +41,6 @@ var ns = {
         console.log(cancion);
         ns.activa=parseInt(cancion);
         console.log(ns.activa);
-        // if (ns.activa==listado.length){
-        //     ns.activa=0;
-        // }
         ns.play();
     },
     // goTo: function(obj){
@@ -39,7 +48,25 @@ var ns = {
     //     ns.play();
     // },
     stop: function(){
-        $('#player').trigger('stop');
+        $('#player').trigger('pause');
+        $('#player').attr('src', '');
+        // $('#player').foreach(function(obj){
+        //     obj.pause();
+        //     obj.currentTime=0;
+        // });
+
+        // $('#player').trigger('stop');
+    },
+    mute: function(){
+        if( $("#player").prop('muted') )
+        {
+            $("#player").prop('muted', false);
+        }
+        else {
+        $("#player").prop('muted', true);
+        }
+        $("#btnMute>i").toggleClass("fas fa-volume-up");
+        $("#btnMute>i").toggleClass("fas fa-volume-mute");
     },
     recargarCanciones(){
         /*canciones=[];
@@ -53,20 +80,26 @@ $(document).ready(function(){
     $('.playcancion').on('click', function(){
         ns.especifica($(this).attr('cancion'));
     });
-    $('#btnPlay').on('click', function(){
-        ns.play();
-    });
     $('#player').on('ended', function(){
         ns.next();
     });
     $('#btnPrevious').on('click', function(){
         ns.previous();
     });
+    $('#btnPause').on('click', function(){
+        ns.pause();
+    });
+    $('#btnPlay').on('click', function(){
+        ns.play();
+    });
     $('#btnNext').on('click', function(){
         ns.next();
     });
     $('#btnStop').on('click', function(){
         ns.stop();
+    });
+    $('#btnMute').on('click', function(){
+        ns.mute();
     });
     $('.btn-song').on('click', function(){
         ns.goTo(this);
